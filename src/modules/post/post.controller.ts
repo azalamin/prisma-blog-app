dab817910b30bc2aab8023ff94f0fbb79dd081cc
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-12-28 01:33:11 +0600

    post route, and get post route done

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,9 +1,37 @@
 import { Request, Response } from "express";
+import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-	console.log({ req, res });
+	try {
+		const result = await PostService.createPost(req.body);
+		res.status(201).json({
+			success: true,
+			data: result,
+		});
+	} catch (error) {
+		res.status(400).json({
+			error: "Post creation failed",
+			details: error,
+		});
+	}
+};
+
+const getPosts = async (req: Request, res: Response) => {
+	try {
+		const result = await PostService.getPosts();
+		res.status(200).json({
+			success: true,
+			data: result,
+		});
+	} catch (error) {
+		res.status(400).json({
+			error: "Post retrieve failed",
+			details: error,
+		});
+	}
 };
 
 export const PostController = {
 	createPost,
+	getPosts,
 };
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -4,5 +4,6 @@ import { PostController } from "./post.controller";
 const router = Router();
 
 router.post("/", PostController.createPost);
+router.get("/", PostController.getPosts);
 
 export const postRouter = router;
