5649a63160f54cd463891f01acc3d775f54e4d32
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2025-12-29 14:54:39 +0600

    user created with better auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import express, { Application } from "express";
+
+import { auth } from "./lib/auth";
 import { postRouter } from "./modules/post/post.router";
 
 const app: Application = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(cors());
 app.use(express.json());
