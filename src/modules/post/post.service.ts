b125e9ad8e1d999c95f66e1b9d401383ed94c3bd
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-04 17:22:06 +0600

    feat(post) view count and get post by id

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -123,8 +123,20 @@ const getPosts = async ({
 };
 
 const getPostById = async (postId: string) => {
-	const result = await prisma.post.findUnique({ where: { id: postId } });
-	return result;
+	return await prisma.$transaction(async tx => {
+		await tx.post.update({
+			where: {
+				id: postId,
+			},
+			data: {
+				views: {
+					increment: 1,
+				},
+			},
+		});
+		const postData = await tx.post.findUnique({ where: { id: postId } });
+		return postData;
+	});
 };
 
 export const PostService = {
